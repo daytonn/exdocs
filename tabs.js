cd49345f8a61e3c5bbd6b149ab8e32968fee64cc
Dayton Nolan <dnolan@cars.com>
2024-08-14 09:18:49 -0700

    Add faker and ecto cooler

--- tabs.js
+++ tabs.js
@@ -1,67 +1,65 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-  const hash = location.hash
-  const tabs = Array.from(document.querySelectorAll(".tabs li a"))
-  const panes = Array.from(document.querySelectorAll(".pane"))
-  const firstPane = panes[0]
-  const firstPaneTitle = tabs[0].innerText
+  const hash = location.hash;
+  const tabs = Array.from(document.querySelectorAll(".tabs li a"));
+  const panes = Array.from(document.querySelectorAll(".pane"));
+  const firstPane = panes[0];
+  const firstPaneTitle = tabs[0].innerText;
 
   function setTitle(string) {
-    const suffix = "QuickDox"
-    const title = string ? `${string} | QuickDox` : suffix
+    const suffix = "QuickDox";
+    const title = string ? `${string} | QuickDox` : suffix;
 
-    document.title = title
+    document.title = title;
   }
 
   function stripHash(hash) {
-    return hash.replace(/^#/, "")
+    return hash.replace(/^#/, "");
   }
 
   function deactivteAllTabs() {
-    tabs.forEach(t => t.classList.remove("active"))
+    tabs.forEach((t) => t.classList.remove("active"));
   }
 
   function hideAllPanes() {
-    deactivteAllTabs()
-    panes.forEach(pane => pane.style.display = "none")
+    deactivteAllTabs();
+    panes.forEach((pane) => (pane.style.display = "none"));
   }
 
   function showPane(pane) {
-    hideAllPanes()
-    const tab = getTab(pane)
+    hideAllPanes();
+    const tab = getTab(pane);
 
-    pane.style.display = "block"
-    pane.querySelector("iframe").style.width = "100%"
-    tab.classList.add("active")
+    pane.style.display = "block";
+    pane.querySelector("iframe").style.width = "100%";
+    tab.classList.add("active");
   }
 
   function getPane(tab) {
-    const id = stripHash(tab.getAttribute("href"))
-    return document.getElementById(id)
+    const id = stripHash(tab.getAttribute("href"));
+    return document.getElementById(id);
   }
 
   function getTab(pane) {
-    const href = `#${pane.getAttribute("id")}`
-    return document.querySelector(`.tabs a[href='${href}']`)
+    const href = `#${pane.getAttribute("id")}`;
+    return document.querySelector(`.tabs a[href='${href}']`);
   }
 
-  tabs.forEach(tab => {
+  tabs.forEach((tab) => {
     tab.addEventListener("click", () => {
-      showPane(getPane(tab))
-      setTitle(tab.innerText)
-    })
-  })
+      showPane(getPane(tab));
+      setTitle(tab.innerText);
+    });
+  });
 
-  showPane(firstPane)
-  setTitle(firstPaneTitle)
+  showPane(firstPane);
+  setTitle(firstPaneTitle);
 
   if (hash) {
-    const id = stripHash(hash)
-    const pane = document.getElementById(id)
-    const title = getTab(pane).innerText
+    const id = stripHash(hash);
+    const pane = document.getElementById(id);
+    const title = getTab(pane).innerText;
 
-    showPane(pane)
-    setTitle(title)
+    showPane(pane);
+    setTitle(title);
   }
-
-  console.log("WTF")
-})
+});
